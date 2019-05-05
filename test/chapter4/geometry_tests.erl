2a26aae04b51d831c91785d4afa4b980368c1690
Andrew Huth <andrew@huth.me>
2019-05-05 12:17:00 -0700

    Add circles to our geometry calculations

--- test/chapter4/geometry_tests.erl
+++ test/chapter4/geometry_tests.erl
@@ -4,5 +4,9 @@
 area_test() ->
   Rectangle = {rectangle, 5, 6},
   Square = {square, 4},
+  Circle = {circle, 3},
   ?assertEqual(geometry:area(Rectangle), 30),
-  ?assertEqual(geometry:area(Square), 16).
+  ?assertEqual(geometry:area(Square), 16),
+  % There's no assert macro that I can see which will handle floats. Instead,
+  % figure out if we're close enough to the expected value.
+  true = abs(geometry:area(Circle) - 28.2743) =< 0.01.
